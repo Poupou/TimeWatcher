71b2963859c81a4650d7320ef1ea53a9ab43a23c
Laurent Senta <laurent.senta@gmail.com>
2012-11-03 20:11:49 +0100

    add opencv support

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,20 @@
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
 
-int main(void)
+int main(int argc, char** argv)
 {
 	std::cout << "Hello World!" << std::endl;
 
 #ifdef DEBUG
 	std::cout << "Hello World! (with debug)" << std::endl;
 #endif
-	return 0;
+
+  std::string filename (argv[1]);
+  cv::Mat img = cv::imread(filename, 0);
+
+  cv::namedWindow("image", CV_WINDOW_AUTOSIZE);
+  cv::imshow("image", img);
+  cv::waitKey();
+  return 0;
 }
