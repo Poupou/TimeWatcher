6d8abff298e639536abd6dcf46f5e1fc8469d2ef
Laurent Senta <laurent.senta@gmail.com>
2012-11-05 23:33:37 +0100

    5 tests

--- tests/data.py
+++ tests/data.py
@@ -1,10 +1,18 @@
 DATA = {
     "detect":
     {
-        "registry-office.jpg": [[638, 678, 115, 115, 0]],
+        "basic-1": [[198, 132, 96, 96, 0]],
+        "registry-office": [[638, 678, 115, 115, 0]],
+        "belfast": [[266, 389, 65, 65, 0]],
+        "big-ben-2": [[397, 399, 124, 124, 0]],
+        "clock-1": [[247, 135, 128, 128, 0]],
     },
     "read":
     {
-        "registry-office.jpg": "01:04",
+        "basic-1": "02:50",
+        "registry-office": "01:04",
+        "belfast": "11:35",
+        "big-ben-2": "05:43",
+        "clock-1": "01:01",
     }
 }
